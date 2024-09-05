a7d953c26752ff0af37d2fef2ea11c0def14d572
termon <a.mccaughey@ulster.ac.uk>
2024-09-05 09:03:59 +0100

    added active class to nav-link

--- src/resources/views/components/nav/link.blade.php
+++ src/resources/views/components/nav/link.blade.php
@@ -1,4 +1,4 @@
-<a {{ $attributes->merge(['class' => 'transition-colors duration-300 text-gray-700 hover:text-gray-900 border-b-2 border-transparent hover:border-black']) }}>
+@props(['name'=>null])
+<a {{ $attributes->merge(['class' => 'transition-colors duration-300 text-gray-700 hover:text-gray-900 border-b-2 border-transparent hover:border-black'])->class(['font-semibold' => Route::is($name)]) }}>  
    {{ $slot }}
 </a>
-
