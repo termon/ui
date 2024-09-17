b661965271abba00898bfeb1a74f09533a3801fc
termon <a.mccaughey@ulster.ac.uk>
2024-09-17 10:10:37 +0100

    added nav button

---
+++ src/resources/views/components/nav/button.blade.php
@@ -0,0 +1,4 @@
+<button {{ $attributes->merge(['class' => 'transition-colors duration-300 text-gray-700 hover:text-gray-900 hover:rounded-md hover:font-semibold block hover:bg-gray-200 whitespace-no-wrap py-2 px-4']) }}>  
+    {{ $slot }}
+ </button>
+
