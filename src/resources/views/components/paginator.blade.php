45c7293be2c6c124d5591fa74d53880f06720a83
Aiden McCaughey <a.mccaughey@ulster.ac.uk>
2025-04-01 13:50:17 +0100

    Update paginator to retain existing query parameters

--- src/resources/views/components/paginator.blade.php
+++ src/resources/views/components/paginator.blade.php
@@ -5,8 +5,11 @@
 @endphp
 <div class="flex items-center gap-2 p-4">
     @if ($items instanceof \Illuminate\Pagination\AbstractPaginator )        
-        {{ $items->appends(request()->query())->links('pagination::tailwind') }}
-        <form method="get" action="{{ request()->fullUrlWithQuery(['size' => request()->get('size', $size)]) }}" class="flex items-center gap-2">
+        {{ $items->appends(request()->query())->links('pagination::tailwind') }}       
+        <form method="get" action="{{ request()->url() }}" class="flex items-center gap-2">
+            @foreach(request()->except(['size', 'page']) as $key => $value)
+                <input type="hidden" name="{{ $key }}" value="{{ $value }}">
+            @endforeach
             <label for="size" class="text-sm text-nowrap">Items per page</label>
             <select id="size" name="size" onchange="this.form.submit()"
                 {{ $attributes->merge(['class' => 'border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5']) }}>
